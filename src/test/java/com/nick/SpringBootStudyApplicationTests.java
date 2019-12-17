294cea8b5b9c0b23b10543f23c628efc846465a6
nick <735937076@qq.com>
2019-12-17 22:19:34 +0800

    init project

---
+++ src/test/java/com/nick/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
